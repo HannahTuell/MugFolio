6e87c12423c31d217c6cfa973e7511be6a6a1698
Ashley <ashley.morris@colorado.edu>
2013-11-06 14:38:15 -0800

    Int commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MugFolio::Application.config.secret_token = 'b8ea80b6b26a29bcdc6b81f71022db3c6c9ae3b621afa4d7bad4da52fc8c82e3ee22c4e76215e6199dde3f0ca203139c606326ff1c05cae146585c15ca2cb77f'
