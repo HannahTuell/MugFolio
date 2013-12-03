252ecd216f0ccda2aaafbb820cc715d3efa23914
Hannah Tuell <hannah.tuell@colorado.edu>
2013-12-02 23:39:11 -0800

    1)User related stuff. DONE -- user can sign up, sign in through navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MugFolio::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+		get "users/sign_out" => "devise/sessions#destroy"
+	end
 
   get "home/index"
   root to: 'home#index'
